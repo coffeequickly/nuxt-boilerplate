3626dfba78f284c921759b36e8f81ca4baa3439a
coffeequickly <jangwonseok.js@gmail.com>
2021-03-17 22:10:10 +0900

    기본 전역 SCSS 추가, svg-inline 등 플러그인 관련 라이브러리, 설정 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,29 +9,34 @@ export default {
       lang: 'en'
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'normalize.css/normalize.css',
+    {
+      src: '~/assets/css/common.scss',
+      lang: 'scss'
+    }
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    {src: '~plugins/svg-inline.js', mode: 'client'},
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -44,5 +49,28 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    publicPath: '/_core/',
+    extend(config, {isDev, isClient, isServer}) {
+      // Store Vue loaders
+      const vueLoader = config.module.rules.find(function (module) {
+        return module.test.toString() === '/\\.vue$/i'
+      })
+
+      // Remove SVG from default rules
+      config.module.rules.forEach((rule) => {
+        if (rule.test.toString() === '/\\.(png|jpe?g|gif|svg|webp|avif)$/i') {
+          rule.test = /\.(png|jpe?g|gif|webp)$/i
+        }
+      })
+
+      // Add svg inline loader configuration
+      config.module.rules.push({
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      })
+
+      // Important to apply transforms on svg-inline:src
+      vueLoader.options.transformAssetUrls['svg-inline'] = 'src'
+    }
   }
 }
